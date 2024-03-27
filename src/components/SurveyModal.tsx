ac5d13cdd719d601342cbbaf0df459e24dd0620b
ChaMin <kusoft1997@gmail.com>
2024-03-28 00:16:32 +0900

    Fix: 불필요한 주석 및 코드 제거

--- src/components/SurveyModal.tsx
+++ src/components/SurveyModal.tsx
@@ -1,5 +1,4 @@
-import React, { SetStateAction, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import SurveyTag from '@components/SurveyTag';
 import { v4 as uuidv4 } from 'uuid';
 
